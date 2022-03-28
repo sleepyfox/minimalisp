f3e6aed02ce3edaa3015683d1b45b7043df051ba
Linux User <h.n.varambhia@gmail.com>
2022-03-28 17:27:21 +0000

    added some guides for next steps

--- minimalisp.js
+++ minimalisp.js
@@ -7,12 +7,21 @@ function evaluate(list){
 }
 
 function parse(stringExpression){
+  // let firstChar = Stringexpression.charCodeAt(0),
+  //     type = determineType(firstChar);
+      
+  // switch (type) {
+  // case 'string':
+  // case 'number':
+  // case 'expression':
+  // }
+  
   if (stringExpression === '') {
     return([])
   } else {
     // an atom e.g. 1 or "string" or symbol (bare word)
     // or an expression (...)
-    if(stringExpression.charCodeAt(1) == 99) {
+    if(stringExpression.charCodeAt(1) == 99) { // TODO: Start with factoring this out into a switch above
       return(['cat'])
     }
     sign = stringExpression[1] // skips over initial '('
