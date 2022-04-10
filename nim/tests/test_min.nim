a049252cfbbbbf4d3d003b637764bd0767f169dc
Nigel Runnels-Moss <sleepyfox@gmail.com>
2022-04-10 08:16:38 +0100

    parses a string with two atoms

--- nim/tests/test_min.nim
+++ nim/tests/test_min.nim
@@ -4,6 +4,9 @@ import minimalisp
 suite "A minimalisp parser":
   test "should parse the empty string as an empty list":
     check(parse("") == [])
-  test "should parse a string with one atom as a list with one element":
+  test "should parse a string with one atom as a list with one atom":
     check(parse("a").len == 1)
     check(parse("a") == ["a"])
+  test "should parse 'a b' as a list of two atoms":
+    check(parse("a b").len == 2)
+    check(parse("a b") == ["a", "b"])
