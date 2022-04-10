c9a97c0d124428d2cc5d480c044a8e1e38e14cd0
Nigel Runnels-Moss <sleepyfox@gmail.com>
2022-04-10 07:35:21 +0100

    refactor to adopt a top-down design strategy

--- nim/src/minimalisp.nim
+++ nim/src/minimalisp.nim
@@ -1,8 +1,20 @@
 # Module minimalisp
 
+proc space_parens(s: string): string =
+  s
+
+proc chunk(s: string): seq[string] =
+  newSeq[string]()
+
+proc tokenise(chunks: seq[string]): seq[string] =
+  chunks
+
 proc parse*(s: string): seq[string] =
   # Surround parens by spaces
+  let spaced_string = space_parens(s)
   # Split input string into chunks
+  let chunks = chunk(spaced_string)
   # Turn chunks into tokens
+  let tokens = tokenise(chunks)
   # Return list of tokens
-  newSeq[string]()
+  tokens
